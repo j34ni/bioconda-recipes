fad1f8058391fdfcc29cd32b6114734185c20a29
j34ni <jeani@uio.no>
2025-02-19 07:30:04 +0000

    Not working

--- recipes/rocm-hip/build.sh
+++ recipes/rocm-hip/build.sh
@@ -2,21 +2,34 @@
 
 set -ex
 
-mkdir -p build
+export HIP_COMMON_DIR=hipamd
+export HIPCC_BIN_DIR=$PREFIX/bin
+export HIP_PLATFORM=amd
 
+echo "6.3.2" > $HIP_COMMON_DIR/VERSION
+ls -la $HIP_COMMON_DIR/VERSION
+cat $HIP_COMMON_DIR/VERSION
+
+if [ ! -e "$HIP_COMMON_DIR/include/hip/hip_runtime.h" ]; then
+  mkdir -p $HIP_COMMON_DIR/include/hip
+  ln -s amd_detail/amd_hip_runtime.h $HIP_COMMON_DIR/include/hip/hip_runtime.h
+fi
+
+cd $HIP_COMMON_DIR
+
+mkdir -p build
 cd build
 
-cmake -G Ninja \
+cmake .. \
+  -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DLLVM_TARGETS_TO_BUILD="AMDGPU;X86" \
-  -DLLVM_ENABLE_ASSERTIONS=OFF \
-  -DHIP_COMPILER=clang \
-  -DHIP_PLATFORM=amd \
-  -DCMAKE_CXX_STANDARD=17 \
+  -DHIP_COMMON_DIR=$HIP_COMMON_DIR \
+  -DHIPCC_BIN_DIR=$HIPCC_BIN_DIR \
+  -DROCM_PATH=$PREFIX \
   -DBUILD_SHARED_LIBS=ON \
-  ..
+  -DCMAKE_PREFIX_PATH="$PREFIX:$HIP_COMMON_DIR/include/hip/amd_detail:$HIP_COMMON_DIR/include/hip" \
+  -DCMAKE_INCLUDE_PATH="$HIP_COMMON_DIR/include/hip/amd_detail:$HIP_COMMON_DIR/include/hip"
 
-ninja -j$(nproc)
-
+ninja
 ninja install
