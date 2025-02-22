262a248bcff55a9bc1cca3bef42a64d0c7980706
j34ni <jeani@uio.no>
2025-02-22 18:54:36 +0000

    Not working yet

--- recipes/hip-runtime-amd/build.sh
+++ recipes/hip-runtime-amd/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -ex
 
-export CC=clang
-export CXX=clang++
-
-export ROCM_PATH=$PREFIX
 export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+export HIP_COMMON_DIR=$CONDA_PREFIX/opt/rocm/include/hip
 
 cd hipamd
 
@@ -16,11 +13,14 @@ cmake -G Ninja \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DROCM_PATH=$PREFIX/opt/rocm \
       -DHIP_PLATFORM=amd \
-      -DHIP_COMMON_DIR=$SRC_DIR \
+      -DHIP_COMMON_DIR=$HIP_COMMON_DIR \
       -DCLR_BUILD_HIP=ON \
       -DCLR_BUILD_OCL=OFF \
-      -DCMAKE_C_COMPILER=$CC \
-      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_C_COMPILER=$CONDA_PREFIX/bin/clang \
+      -DCMAKE_CXX_COMPILER=$CONDA_PREFIX/bin/clang++ \
+      -DCLANG_RESOURCE_DIR=$CONDA_PREFIX/lib/clang/18 \
+      -DHIP_CXX_FLAGS="-I$HIP_COMMON_DIR" \
+      -DHIP_C_FLAGS="-I$HIP_COMMON_DIR" \
       ..
 
 ninja -j$(nproc)
